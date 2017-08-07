471240fb2b15cacd4a92cd3ca92b3d05e1072427
plai <plyhin@hotmail.com>
2017-08-08 00:45:25 +0700

    Refactor

--- web/src/main/java/com/oasw/web/MainController.java
+++ web/src/main/java/com/oasw/web/MainController.java
@@ -1,7 +1,5 @@
 package com.oasw.web;
 
-import com.oasw.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
